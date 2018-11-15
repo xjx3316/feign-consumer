b9f40cc2fb6e7f4022ea9e4867beb42234c8677d
xjx <xjx@qq.com>
2018-11-15 18:10:41 +0800

    xjx

---
+++ src/main/java/com/didispace/feignconsumer/controller/ConsumerController.java
@@ -0,0 +1,34 @@
+package com.didispace.feignconsumer.controller;
+
+import com.didispace.feignconsumer.pojo.User;
+import com.didispace.feignconsumer.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xjx on 2018/11/15.
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @RequestMapping(value = "/feign-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return helloService.hello();
+    }
+
+    @RequestMapping(value = "/feign-consumer2", method = RequestMethod.GET)
+    public String helloConsumer2() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(helloService.hello()).append("</br>");
+        sb.append(helloService.hello("DIDI")).append("</br>");
+        sb.append(helloService.hello("DIDI", 15)).append("</br>");
+        sb.append(helloService.hello(new User("DIDI", 18))).append("</br>");
+        return sb.toString();
+    }
+
+}
